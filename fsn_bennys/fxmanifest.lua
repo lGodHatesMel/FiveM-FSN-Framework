97e85039f72921301e2866033a1cc0beed10b79b
Buggz Evol <56521233+lGodHatesMel@users.noreply.github.com>
2020-04-17 21:39:41 -0400

    update to fxmanifest

--- fsn_bennys/fxmanifest.lua
+++ fsn_bennys/fxmanifest.lua
@@ -1,14 +1,12 @@
---[[/	:FSN:	\]]--
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'bodacious'
+games { 'gta5' }
+
 client_script '@fsn_main/cl_utils.lua'
 server_script '@fsn_main/sv_utils.lua'
 client_script '@fsn_main/server_settings/sh_settings.lua'
 server_script '@fsn_main/server_settings/sh_settings.lua'
 server_script '@mysql-async/lib/MySQL.lua'
---[[/	:FSN:	\]]--
 
 client_script 'menu.lua'
 client_script 'cl_config.lua'
 client_script 'cl_bennys.lua'
-
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
