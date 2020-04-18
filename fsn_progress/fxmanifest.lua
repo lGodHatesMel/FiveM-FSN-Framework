8d0f08b9649a5c0fda46115f3b832be93c024b4c
Buggz Evol <56521233+lGodHatesMel@users.noreply.github.com>
2020-04-17 22:40:01 -0400

    changed to fxmanifest

--- fsn_progress/fxmanifest.lua
+++ fsn_progress/fxmanifest.lua
@@ -1,15 +1,14 @@
---[[/	:FSN:	\]]--
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'bodacious'
+games { 'gta5' }
+
 client_script '@fsn_main/cl_utils.lua'
 server_script '@fsn_main/sv_utils.lua'
 client_script '@fsn_main/server_settings/sh_settings.lua'
 server_script '@fsn_main/server_settings/sh_settings.lua'
 server_script '@mysql-async/lib/MySQL.lua'
---[[/	:FSN:	\]]--
 
 client_script 'client.lua'
 
--- exports
 exports {
 	"fsn_ProgressBar",
 	"removeBar"
