dc03a46e7eee66981f29ac330cf829a74cde15c9
Buggz Evol <56521233+lGodHatesMel@users.noreply.github.com>
2020-04-17 22:24:02 -0400

    chnaged to fxmanifest.lua

--- fsn_notify/fxmanifest.lua
+++ fsn_notify/fxmanifest.lua
@@ -1,11 +1,11 @@
---[[/	:FSN:	\]]--
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'bodacious'
+games { 'gta5' }
+
 client_script '@fsn_main/cl_utils.lua'
 server_script '@fsn_main/sv_utils.lua'
 client_script '@fsn_main/server_settings/sh_settings.lua'
 server_script '@fsn_main/server_settings/sh_settings.lua'
 server_script '@mysql-async/lib/MySQL.lua'
---[[/	:FSN:	\]]--
 
 description "Simple Notification Script using https://notifyjs.com/"
 
